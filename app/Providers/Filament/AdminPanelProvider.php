25cbd9dffd766f8775b99ff57d713529cb996b53
luisarz <svcomputec@gmail.com>
2025-10-10 15:12:23 -0600

    payment cuentas por cobrar

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -65,7 +65,7 @@ class AdminPanelProvider extends PanelProvider
             ->authGuard('web')
             ->sidebarCollapsibleOnDesktop()
 //            ->sidebarAccordion()
-            ->topbar(false)
+//            ->topbar(false)
 
         ->databaseNotifications()
             ->login(CustomLogin::class)
